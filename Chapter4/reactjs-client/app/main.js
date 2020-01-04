2c98e4dd5dc93a7cec2435f3c5d03dbc374beab4
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-01-04 11:04:42 +0700

    feat(react): client connect with running local server and make requset through http protocol

---
+++ Chapter4/reactjs-client/app/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+import EmployeeApp from "./components/employee-app.jsx";
+
+ReactDOM.render(<EmployeeApp />, document.getElementById("react"));
