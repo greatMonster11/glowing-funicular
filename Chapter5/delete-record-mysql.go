61415f99e4cd353bc20d0d3d56a9b3550f539803
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-01-06 10:26:32 +0700

    bugs(typo): update connection with database

--- Chapter5/delete-record-mysql.go
+++ Chapter5/delete-record-mysql.go
@@ -23,8 +23,7 @@ var connectionError error
 func init() {
 	db, connectionError = sql.Open(DRIVER_NAME, DATA_SOURCE_NAME)
 	if connectionError != nil {
-		log.Fatal("error connecting to database ::", connectionError)
-		return
+		log.Fatal("error connecting to database :: ", connectionError)
 	}
 }
 
@@ -35,32 +34,28 @@ func deleteRecord(w http.ResponseWriter, r *http.Request) {
 		log.Print("going to delete record in database for name :: ", name[0])
 		stmt, err := db.Prepare("DELETE from employee where name=?")
 		if err != nil {
-			log.Printf("error occurred while preparing query :: ", err)
+			log.Print("error occurred while preparing query :: ", err)
 			return
 		}
-
 		result, err := stmt.Exec(name[0])
-
 		if err != nil {
-			log.Printf("error occurred while executing :: ", err)
+			log.Print("error occurred while executing query :: ", err)
 			return
 		}
-
 		rowsAffected, err := result.RowsAffected()
 		fmt.Fprintf(w, "Number of rows deleted in database are :: %d", rowsAffected)
 	} else {
-		fmt.Fprint(w, "Error occurred while deleting record in database for name %s", name[0])
+		fmt.Fprintf(w, "Error occurred while deleting record in database for name %s", name[0])
 	}
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("employee/delete", deleteRecord).Methods("DELETE")
-
+	router.HandleFunc("/employee/delete", deleteRecord).Methods("DELETE")
 	defer db.Close()
 	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, router)
 	if err != nil {
-		log.Fatal("error starting http server:", err)
+		log.Fatal("error starting http server :: ", err)
 		return
 	}
 }
