0e45905aa5b1a3e8bf643f3267e0af14a7fafbbf
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-01-10 15:00:28 +0700

    feat(beego): bootstrap project with bee go

---
+++ Chapter8/my-first-beego-project/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "my-first-beego-project/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ Chapter8/my-first-beego-project/routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"my-first-beego-project/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
